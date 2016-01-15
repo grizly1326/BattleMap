0e400a9b4c4744f0a7aad5b595ea29248cf7f5dd
grizly1326 <daniel.maly@hotmail.com>
2016-01-15 14:36:49 +0100

    15.1.2016 commit

--- lesson3/src/Threads/ServerT.java
+++ lesson3/src/Threads/ServerT.java
@@ -3,16 +3,11 @@ package Threads;
 import Server.Server;
 
 public class ServerT implements Runnable {
-	Server a;
-	boolean toggle=false;
-	public ServerT(boolean toggle){
-		this.toggle= toggle;
-		Thread t= new Thread(this);
-		t.start();
+	public ServerT(){
 	}
 	public void run(){
-		while(toggle){
-			a= new Server();
+		while(true){
+			Server a= new Server();
 			a.create(8888);
 			a.Listener(true,null);
 		}
